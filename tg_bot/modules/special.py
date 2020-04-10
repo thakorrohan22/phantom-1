eede6784d8af91b02fb3562f65b330ecd12af6cf
leobrownlee <54571037+leobrownlee@users.noreply.github.com>
2020-04-10 07:40:33 +0530

    Update special.py

--- tg_bot/modules/special.py
+++ tg_bot/modules/special.py
@@ -134,29 +134,12 @@ def slist(bot: Bot, update: Update):
     message.reply_text(text2 + "\n", parse_mode=ParseMode.MARKDOWN)
 
 __help__ = """
-**Owner only:**
-- /getlink **chatid**: Get the invite link for a specific chat.
-- /banall: Ban all members from a chat
-- /snipe **chatid** **string**: Make me send a message to a specific chat.
-- /leavechat **chatid** : leave a chat
-**Sudo/owner only:**
-- /quickscope **userid** **chatid**: Ban user from chat.
-- /quickunban **userid** **chatid**: Unban user from chat.
-- 
-- 
-- 
+
 - /Stats: check bot's stats
 - /chatlist: get chatlist
 - /gbanlist: get gbanned users list
-
-- Chat bans via /restrict chat_id and /unrestrict chat_id commands
-**Support user:**
 - /Gban : Global ban a user
 - /Ungban : Ungban a user
-- 
-- 
-Sudo/owner can use these commands too.
-**Users:**
 - /slist Gives a list of sudo and support users
 """
 
