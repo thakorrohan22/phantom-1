66f007381adda2acfa40270f0fcab0c32bb9b7f2
leobrownlee <54571037+leobrownlee@users.noreply.github.com>
2020-04-13 09:29:35 +0530

    Update welcome_sql.py

--- tg_bot/modules/sql/welcome_sql.py
+++ tg_bot/modules/sql/welcome_sql.py
@@ -1,4 +1,3 @@
-import random
 import threading
 
 from sqlalchemy import Column, String, Boolean, UnicodeText, Integer, BigInteger
@@ -6,95 +5,8 @@ from sqlalchemy import Column, String, Boolean, UnicodeText, Integer, BigInteger
 from tg_bot.modules.helper_funcs.msg_types import Types
 from tg_bot.modules.sql import SESSION, BASE
 
-DEFAULT_WELCOME = 'Hey {first}, how are you?'
-DEFAULT_GOODBYE = 'Nice knowing ya!'
-
-DEFAULT_WELCOME_MESSAGES = [
-    "{first} is here!",
-    "Ready player {first}",
-    "Genos, {first} is here.",
-    "A wild {first} appeared.",
-    "{first} came in like a Lion!",
-    "{first} has joined your party.",
-    "{first} just joined. Can I get a heal?",
-    "{first} just joined the chat - asdgfhak!",
-    "{first} just joined. Everyone, look busy!",
-    "Welcome, {first}. Stay awhile and listen.",
-    "Welcome, {first}. We were expecting you ( ͡° ͜ʖ ͡°)",
-    "Welcome, {first}. We hope you brought pizza.",
-    "Welcome, {first}. Leave your weapons by the door.",
-    "Swoooosh. {first} just landed.",
-    "Brace yourselves. {first} just joined the chat.",
-    "{first} just joined. Hide your bananas.",
-    "{first} just arrived. Seems OP - please nerf.",
-    "{first} just slid into the chat.",
-    "A {first} has spawned in the chat.",
-    "Big {first} showed up!",
-    "Where’s {first}? In the chat!",
-    "{first} hopped into the chat. Kangaroo!!",
-    "{first} just showed up. Hold my beer.",
-    "Challenger approaching! {first} has appeared!",
-    "It's a bird! It's a plane! Nevermind, it's just {first}.",
-    "It's {first}! Praise the sun! \o/",
-    "Never gonna give {first} up. Never gonna let {first} down.",
-    "Ha! {first} has joined! You activated my trap card!",
-    "Cheers, love! {first}'s here!",
-    "Hey! Listen! {first} has joined!",
-    "We've been expecting you {first}",
-    "It's dangerous to go alone, take {first}!",
-    "{first} has joined the chat! It's super effective!",
-    "Cheers, love! {first} is here!",
-    "{first} is here, as the prophecy foretold.",
-    "{first} has arrived. Party's over.",
-    "{first} is here to kick butt and chew bubblegum. And {first} is all out of gum.",
-    "Hello. Is it {first} you're looking for?",
-    "{first} has joined. Stay a while and listen!",
-    "Roses are red, violets are blue, {first} joined this chat with you",
-    "Welcome {first}, Avoid Punches if you can!",
-    "It's a bird! It's a plane! - Nope, its {first}!",
-    "{first} Joined! - Ok.",
-    "All Hail {first}!",
-    "Hi, {first}. Don't lurk, Only Villans do that.",
-    "{first} has joined the battle bus.",
-    "A new Challenger enters!",
-    "Ok!",
-    "{first} just fell into the chat!",
-    "Something just fell from the sky! - oh, its {first}.",
-    "{first} Just teleported into the chat!",
-    "Hi, {first}, show me your Hunter License!",
-    "I'm looking for Garo, oh wait nvm it's {first}.",
-    "Welcome {first}, Leaving is not an option!",
-    "Run Forest! ..I mean...{first}.",
-    "{first} do 100 push-ups, 100 sit-ups, 100 squats, and a 10km running EVERY SINGLE DAY!!!",
-    "Huh?\nDid someone with a disaster level just join?\nOh wait, it's just {first}.",
-    "Hey, {first}, ever heard the King Engine?",
-    "Hey, {first}, Empty your pockets.",
-    "Hey, {first}!, Are you strong?",
-    "Call the Avengers! - {first} just joined the chat.",
-    "{first} joined. You must construct additional pylons.",
-    "Ermagherd. {first} is here.",
-]
-DEFAULT_GOODBYE_MESSAGES = [
-    "{first} will be missed.",
-    "{first} just went offline.",
-    "{first} has left the lobby.",
-    "{first} has left the clan.",
-    "{first} has left the game.",
-    "{first} has fled the area.",
-    "{first} is out of the running.",
-    "Nice knowing ya, {first}!",
-    "It was a fun time {first}.",
-    "We hope to see you again soon, {first}.",
-    "I donut want to say goodbye, {first}.",
-    "Goodbye {first}! Guess who's gonna miss you :')",
-    "Goodbye {first}! It's gonna be lonely without ya.",
-    "Please don't leave me alone in this place, {first}!",
-    "Good luck finding better shitposters than us, {first}!",
-    "You know we're gonna miss you {first}. Right? Right? Right?",
-    "Congratulations, {first}! You're officially free of this mess.",
-    "{first}. You were an opponent worth fighting.",
-    "You're leaving, {first}? Yare Yare Daze.",
-]
+DEFAULT_WELCOME = "Hey {first}, how are you?"
+DEFAULT_GOODBYE = "Nice knowing ya!"
 
 
 class Welcome(BASE):
@@ -103,14 +15,16 @@ class Welcome(BASE):
     should_welcome = Column(Boolean, default=True)
     should_goodbye = Column(Boolean, default=True)
 
-    custom_welcome = Column(UnicodeText, default=random.choice(DEFAULT_WELCOME_MESSAGES))
+    custom_welcome = Column(UnicodeText, default=DEFAULT_WELCOME)
     custom_welcome_caption = Column(UnicodeText, default=None)
     welcome_type = Column(Integer, default=Types.TEXT.value)
 
-    custom_leave = Column(UnicodeText, default=random.choice(DEFAULT_GOODBYE_MESSAGES))
+    custom_leave = Column(UnicodeText, default=DEFAULT_GOODBYE)
     leave_type = Column(Integer, default=Types.TEXT.value)
 
     clean_welcome = Column(BigInteger)
+    del_joined = Column(BigInteger)
+    del_commands = Column(BigInteger)
 
     def __init__(self, chat_id, should_welcome=True, should_goodbye=True):
         self.chat_id = chat_id
@@ -151,114 +65,34 @@ class GoodbyeButtons(BASE):
         self.same_line = same_line
 
 
-class WelcomeMute(BASE):
-    __tablename__ = "welcome_mutes"
-    chat_id = Column(String(14), primary_key=True)
-    welcomemutes = Column(UnicodeText, default=False)
-
-    def __init__(self, chat_id, welcomemutes):
-        self.chat_id = str(chat_id)  # ensure string
-        self.welcomemutes = welcomemutes
-
-
-class WelcomeMuteUsers(BASE):
-    __tablename__ = "human_checks"
-    user_id = Column(Integer, primary_key=True)
-    chat_id = Column(String(14), primary_key=True)
-    human_check = Column(Boolean)
-
-    def __init__(self, user_id, chat_id, human_check):
-        self.user_id = (user_id)  # ensure string
-        self.chat_id = str(chat_id)
-        self.human_check = human_check
-
-
 Welcome.__table__.create(checkfirst=True)
 WelcomeButtons.__table__.create(checkfirst=True)
 GoodbyeButtons.__table__.create(checkfirst=True)
-WelcomeMute.__table__.create(checkfirst=True)
-WelcomeMuteUsers.__table__.create(checkfirst=True)
 
 INSERTION_LOCK = threading.RLock()
 WELC_BTN_LOCK = threading.RLock()
 LEAVE_BTN_LOCK = threading.RLock()
-WM_LOCK = threading.RLock()
-
-
-def welcome_mutes(chat_id):
-    try:
-        welcomemutes = SESSION.query(WelcomeMute).get(str(chat_id))
-        if welcomemutes:
-            return welcomemutes.welcomemutes
-        return False
-    finally:
-        SESSION.close()
-
-
-def set_welcome_mutes(chat_id, welcomemutes):
-    with WM_LOCK:
-        prev = SESSION.query(WelcomeMute).get((str(chat_id)))
-        if prev:
-            SESSION.delete(prev)
-        welcome_m = WelcomeMute(str(chat_id), welcomemutes)
-        SESSION.add(welcome_m)
-        SESSION.commit()
-
-
-def set_human_checks(user_id, chat_id):
-    with INSERTION_LOCK:
-        human_check = SESSION.query(WelcomeMuteUsers).get((user_id, str(chat_id)))
-        if not human_check:
-            human_check = WelcomeMuteUsers(user_id, str(chat_id), True)
-
-        else:
-            human_check.human_check = True
-
-        SESSION.add(human_check)
-        SESSION.commit()
-
-        return human_check
-
-
-def get_human_checks(user_id, chat_id):
-    try:
-        human_check = SESSION.query(WelcomeMuteUsers).get((user_id, str(chat_id)))
-        if not human_check:
-            return None
-        human_check = human_check.human_check
-        return human_check
-    finally:
-        SESSION.close()
-
-
-def get_welc_mutes_pref(chat_id):
-    welcomemutes = SESSION.query(WelcomeMute).get(str(chat_id))
-    SESSION.close()
-
-    if welcomemutes:
-        return welcomemutes.welcomemutes
-
-    return False
 
 
 def get_welc_pref(chat_id):
     welc = SESSION.query(Welcome).get(str(chat_id))
     SESSION.close()
+
     if welc:
-        return welc.should_welcome, welc.custom_welcome, welc.welcome_type
+        return welc.should_welcome, welc.custom_welcome, welc.welcome_type, welc.custom_welcome_caption
     else:
         # Welcome by default.
-        return True, DEFAULT_WELCOME, Types.TEXT
+        return True, DEFAULT_WELCOME, Types.TEXT, None
 
 
 def get_gdbye_pref(chat_id):
     welc = SESSION.query(Welcome).get(str(chat_id))
     SESSION.close()
     if welc:
-        return welc.should_goodbye, welc.custom_leave, welc.leave_type, welc.custom_welcome_caption
+        return welc.should_goodbye, welc.custom_leave, welc.leave_type
     else:
         # Welcome by default.
-        return True, DEFAULT_GOODBYE, Types.TEXT, None
+        return True, DEFAULT_GOODBYE, Types.TEXT
 
 
 def set_clean_welcome(chat_id, clean_welcome):
@@ -279,6 +113,50 @@ def get_clean_pref(chat_id):
 
     if welc:
         return welc.clean_welcome
+
+    return False
+
+
+def set_del_joined(chat_id, del_joined):
+    with INSERTION_LOCK:
+        curr = SESSION.query(Welcome).get(str(chat_id))
+        if not curr:
+            curr = Welcome(str(chat_id))
+
+        curr.del_joined = int(del_joined)
+
+        SESSION.add(curr)
+        SESSION.commit()
+
+
+def get_del_pref(chat_id):
+    welc = SESSION.query(Welcome).get(str(chat_id))
+    SESSION.close()
+
+    if welc:
+        return welc.del_joined
+
+    return False
+
+
+def set_cmd_joined(chat_id, cmd_joined):
+    with INSERTION_LOCK:
+        curr = SESSION.query(Welcome).get(str(chat_id))
+        if not curr:
+            curr = Welcome(str(chat_id))
+
+        curr.del_commands = int(cmd_joined)
+
+        SESSION.add(curr)
+        SESSION.commit()
+
+
+def get_cmd_pref(chat_id):
+    welc = SESSION.query(Welcome).get(str(chat_id))
+    SESSION.close()
+
+    if welc:
+        return welc.del_commands
 
     return False
 
