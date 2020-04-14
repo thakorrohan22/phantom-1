a9918a4c893bbc0d8d7ad42ec9dfc2daa9cfa215
leobrownlee <54571037+leobrownlee@users.noreply.github.com>
2020-04-14 15:22:22 +0530

    Update misc.py

--- tg_bot/modules/misc.py
+++ tg_bot/modules/misc.py
@@ -108,20 +108,20 @@ def info(bot: Bot, update: Update, args: List[str]):
     else:
         return
 
-    text = (f"<b>Characteristics:</b>\n"
-            f"ID: <code>{user.id}</code>\n"
-            f"First Name: {html.escape(user.first_name)}")
+    text = (f"<b>user information:</b>\n"
+            f"🆔️ID: <code>{user.id}</code>\n"
+            f"👤First Name: {html.escape(user.first_name)}")
 
     if user.last_name:
-        text += f"\nLast Name: {html.escape(user.last_name)}"
+        text += f"\n👤Last Name: {html.escape(user.last_name)}"
 
     if user.username:
-        text += f"\nUsername: @{html.escape(user.username)}"
+        text += f"\n👤Username: @{html.escape(user.username)}"
 
-    text += f"\nPermanent user link: {mention_html(user.id, 'link')}"
+    text += f"\n👤Permanent user link: {mention_html(user.id, 'link')}"
 
     num_chats = sql.get_user_num_chats(user.id)
-    text += f"\nChat count: <code>{num_chats}</code>"
+    text += f"\n🌍Chat count: <code>{num_chats}</code>"
 
     try:
         user_member = chat.get_member(user.id)
